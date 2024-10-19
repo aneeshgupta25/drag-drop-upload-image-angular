8ba56d932d789a60cdab75820022ed21aeb35676
aneeshgupta25 <aneeshgupta2512@gmail.com>
2024-10-20 02:05:21 +0530

    Upload Image directive

--- src/app/upload-image/upload-image.component.ts
+++ src/app/upload-image/upload-image.component.ts
@@ -4,11 +4,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule } from '@angular/forms';
 
+import { UploadImageDirective } from './upload-image.directive';
 
 @Component({
   selector: 'app-upload-image',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatIconModule, FormsModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, FormsModule, UploadImageDirective],
   templateUrl: './upload-image.component.html',
   styleUrl: './upload-image.component.scss'
 })
